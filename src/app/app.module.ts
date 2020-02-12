48d8f833decde3351b9608b542cb8b2d2c2fdb10
Degs <davidcalvomeier@protonmail.com>
2020-02-12 23:42:44 +0100

    Install Universal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {NavbarModule} from './components/shared/navbar/navbar.module';
 import {HeroesModule} from './pages/heroes/heroes.module';
@@ -11,13 +11,12 @@ import {HttpClientModule} from '@angular/common/http';
 import {MatButtonModule} from '@angular/material/button';
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({appId: 'universalApp'}),
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
@@ -28,4 +27,5 @@ import {MatButtonModule} from '@angular/material/button';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
